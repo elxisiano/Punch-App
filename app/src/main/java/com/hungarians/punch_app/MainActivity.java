9aea66a534804a071718c033afa7017248678b25
elxisiano <elxisiano@gmail.com>
2016-03-30 19:08:50 +0200

    Tabs have been added, with a different Fragment for each.

--- app/src/main/java/com/hungarians/punch_app/MainActivity.java
+++ app/src/main/java/com/hungarians/punch_app/MainActivity.java
@@ -6,11 +6,18 @@ import android.os.Message;
 import android.os.SystemClock;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Chronometer;
 import android.widget.ArrayAdapter;
@@ -21,8 +28,22 @@ import android.app.Activity;
 import android.content.Intent;
 
 
-
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
 
     // Intent request codes
     private static final int REQUEST_CONNECT_DEVICE_SECURE = 1;
@@ -30,13 +51,13 @@ public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_ENABLE_BT = 3;
 
     // Layout Views
-    public TextView vistaContador;
-    public TextView vistaMensaje;
-    public TextView vistaGForce;
-    public TextView vistaHPS;
-    public TextView vistaTBH;
-    public TextView vistaKcal;
-    public Chronometer vistaChrono;
+    public static TextView vistaContador;
+    public static TextView vistaMensaje;
+    public static TextView vistaGForce;
+    public static TextView vistaHPS;
+    public static TextView vistaTBH;
+    public static TextView vistaKcal;
+    public static Chronometer vistaChrono;
 
     public int numeroGolpes = 0;
 
@@ -74,8 +95,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -88,14 +121,7 @@ public class MainActivity extends AppCompatActivity {
 
         //TODO: Aquí se generan los componentes de la interfaz de usuario
 
-        // Se crean las vistas del contantador y los mensajes
-        vistaContador = (TextView)findViewById(R.id.contadorView);
-        vistaMensaje = (TextView)findViewById(R.id.messageView);
-        vistaGForce = (TextView)findViewById(R.id.gForceView);
-        vistaHPS = (TextView)findViewById(R.id.hpsView);
-        vistaTBH = (TextView)findViewById(R.id.tbhView);
-        vistaKcal = (TextView)findViewById(R.id.kcalView);
-        vistaChrono = (Chronometer)findViewById(R.id.chronometer);
+
 
         //TODO: PASO 1: Obtención del BluetoothAdapter (Se incluye en la memoria)
         //Obtener el adapter local para Bluetooth
@@ -104,6 +130,104 @@ public class MainActivity extends AppCompatActivity {
         if (mBluetoothAdapter == null) {
             Toast.makeText(this, "No existe adapatador de Bluetooth", Toast.LENGTH_LONG).show();
             finish();
+        }
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        private int mSection;
+
+
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            mSection = getArguments().getInt(ARG_SECTION_NUMBER);
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            View rootView;
+
+            if (mSection == 1) {
+                rootView = inflater.inflate(R.layout.fragment_counter, container, false);
+
+                // Se crean las vistas del contantador y los mensajes
+                vistaContador = (TextView)rootView.findViewById(R.id.contadorView);
+                vistaMensaje = (TextView)rootView.findViewById(R.id.messageView);
+                vistaGForce = (TextView)rootView.findViewById(R.id.gForceView);
+                vistaHPS = (TextView)rootView.findViewById(R.id.hpsView);
+                vistaTBH = (TextView)rootView.findViewById(R.id.tbhView);
+                vistaKcal = (TextView)rootView.findViewById(R.id.kcalView);
+                vistaChrono = (Chronometer)rootView.findViewById(R.id.chronometer);
+                //TextView textView = (TextView) rootView.findViewById(R.id.vistaLargoTexto);
+                //textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            }
+            else{
+                rootView = inflater.inflate(R.layout.fragment_sessions, container, false);
+            }
+
+            return rootView;
+        }
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return PlaceholderFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 2 total pages.
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "CONTADOR";
+                case 1:
+                    return "SESIONES";
+            }
+            return null;
         }
     }
 
